c6155efffeaf54e0b9cea1cb70bce6b4c7f8bd46
himedsoso <himed.abidar@gmail.com>
2023-03-14 09:47:43 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/queteOneSpring/QueteOneSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.queteOneSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteOneSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteOneSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/queteOneSpring/QueteOneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.queteOneSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueteOneSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
